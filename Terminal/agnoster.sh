a5c87a7468fb003c5846cd5476cbaccad781de19
AzureAD\SimonCousins <simon@noxcivis.com>
2024-09-06 11:09:48 -0400

    chore: Update Zsh installation script to include automatic package confirmation
    This commit updates the Zsh installation script to include the `-y` flag, which enables automatic confirmation of package installation. This improvement ensures a smoother installation process for users.
    
    Note: The script requires the user to enter their password to install Zsh and change the default shell.
    
    Github repo: [https://github.com/Noxcivis/linux](https://github.com/Noxcivis/linux)
    
    Tested on: Ubuntu 20.04 LTS

--- Terminal/agnoster.sh
+++ Terminal/agnoster.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+# Author: Noxcivis
+# Github repo : https://github.com/Noxcivis
+# Created on: 2021-08-15
+# Last updated on: 2021-08-15
+# Version: 1.0
+# License: MIT
+# Usage: bash agnoster.sh
+# Description: This script configures the Zsh shell with the Agnoster theme and restarts the shell.
+# Github link : https://github.com/Noxcivis/linux/blob/main/Terminal/agnoster.sh
+# Tested on: Ubuntu 20.04 LTS
+# Note: This script requires curl to be installed on the system. If it is not installed, run 'sudo apt install curl -y' before running this script.
+# Note: This script requires the user to enter their password to install Zsh and change the default shell.
 
 # Update package list and install Zsh
 sudo apt update -y
@@ -26,4 +37,5 @@ echo "source ~/.zshrc" | zsh
 # Start a new Zsh session
 exec zsh
 
+# Print success message
 echo "Zsh has been configured with the Agnoster theme and the shell has been restarted."
