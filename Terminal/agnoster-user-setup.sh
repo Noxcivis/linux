8d6bec200cae52a0f166907411196c202f8f7bfa
AzureAD\SimonCousins <simon@noxcivis.com>
2024-09-07 21:14:41 -0400

    refactor: Remove unnecessary shell restart in agnoster terminal theme installation script

--- Terminal/agnoster-user-setup.sh
+++ Terminal/agnoster-user-setup.sh
@@ -10,9 +10,6 @@
 # Description: This script configures the Zsh shell with the Agnoster theme and restarts the shell.
 # Github link : https://github.com/Noxcivis/linux/blob/main/Terminal/agnoster.sh
 # Tested on: Ubuntu 20.04 LTS
-# Note: This script requires curl to be installed on the system. If it is not installed, run 'sudo apt install curl -y' before running this script.
-# Note: This script requires the user to enter their password to install Zsh and change the default shell.
-
 
 rm -rf ~/.oh-my-zsh
 
